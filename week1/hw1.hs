125ed3862c4c1a111911ec25a6b6fd7794508d51
rijgndqw012 <rijgndqw012@gmail.com>
2016-05-19 16:08:06 +0900

    Solved Homework #1

--- week1/hw1.hs
+++ week1/hw1.hs
@@ -12,18 +12,34 @@ toRevDigits num
   | otherwise = []
 
 --Exercise 3
---doubleEveryOther :: [Integer] -> [Integer]
---doubleEveryOther list = [ if 
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther list = calc
+  where isIndexEven l = (length l) `mod` 2  /= 0
+        doubleEvenIndex [] = []
+        doubleEvenIndex l  = (if isIndexEven l then (head l) * 2 else (head l)):doubleEvenIndex (tail l)
+        calc = doubleEvenIndex list
 
 --Exercise 4
 sumDigits :: [Integer] -> Integer
-sumDigits list = sum (map (sum . toRevDigits) list))
+sumDigits list = sum (map (sum . toRevDigits) list)
 
 --Exercise 5
---luhn :: Integer -> Bool
-
+luhn :: Integer -> Bool
+luhn n
+  | n  > 0 = if sumDigits (doubleEveryOther (toRevDigits  n) ) `mod` 10 == 0 then True else False
+  | otherwise = error "zero or negative number"
 
 --Exercise 6
-
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (n-1) c b a)
 
 --Exercise 7
+hanoi2 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi2 0 _ _ _ _ = []
+hanoi2 1 a b _ _ = [(a,b)]
+hanoi2 n a b c d = (hanoi2 k a d b c) ++ (hanoi (n-k) a b c) ++ (hanoi2 k d b a c)
+  where k = (round . sqrt . fromInteger) (2*n+1) - 1 
--- week1/hw2.hs
+++ week1/hw2.hs
@@ -1,2 +1,55 @@
+import Data.List
+
+data Peg = Red | Green | Blue | Yellow | Orange | Purple
+         deriving (Show, Eq, Ord)
+
+type Code = [Peg]
+
+data Move = Move Code Int Int
+          deriving (Show, Eq)
+
+colors :: [Peg]
+colors = [Red, Green, Blue, Yellow, Orange, Purple]
+
+
 --Exercise 1
---exactMatches :: Code -> Code -> Int
+exactMatches :: Code -> Code -> Int
+exactMatches [] [] = 0
+exactMatches (x:xs) (y:ys) = result + exactMatches xs ys
+  where result = if x == y then 1 else 0
+
+--Exercise 2
+countColors :: Code -> [Int]
+countColors [] = [0]
+countColors list = map (\x -> wordcount list x) colors
+  where wordcount :: Code -> Peg -> Int
+        wordcount [] w = 0
+        wordcount (x:xs) w = (if x == w then 1 else 0) + wordcount xs w
+
+matches :: [Peg] -> [Peg] -> Int
+matches [] [] = 0
+matches list1 list2 = matchNumber (countColors list1) (countColors list2) 
+  where matchNumber :: [Int] -> [Int] -> Int
+        matchNumber [] [] = 0
+        matchNumber (x:xs) (y:ys) = (if x==y then x else min'(x,y) ) + matchNumber xs ys
+          where min' :: (Ord a) => (a, a) -> a
+                min' (x, y) = if x > y then y else x
+
+--Exercise 3
+getMove :: Code -> Code -> Move
+getMove l1 l2 = Move l2 (exactNum l1 l2) ((matches l1 l2)-exactNum l1 l2)
+  where exactNum [] [] = 0
+        exactNum (x:xs) (y:ys) = (if x == y then 1 else 0) + exactNum xs ys
+
+--Exercise 4
+isConsistent :: Move -> Code -> Bool
+isConsistent (Move code1 exact nonExact) code2 = (exact == exactNum code1 code2 && nonExact == (matches code1 code2) - exactNum code1 code2) 
+  where exactNum [] [] = 0
+        exactNum (x:xs) (y:ys) = (if x == y then 1 else 0) + exactNum xs ys
+
+--Exercise 5
+filterCodes :: Move -> [Code] -> [Code]
+filterCodes m codes = filter (isConsistent m) codes
+
+--Exercise 6
+allCodes :: Int -> [Code]
